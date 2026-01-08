bc85db77346ea565d18e2d1f98689325c1262de9
Daniel Kulichkov <danchick03@gmail.com>
2026-01-08 12:46:11 +0300

    feat(api): add db pool package

---
+++ apps/api/internal/db/pool.go
@@ -0,0 +1,34 @@
+package db
+
+import (
+	"cargorun/internal/config"
+	"context"
+	"log/slog"
+	"sync"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+var (
+	pool    *pgxpool.Pool
+	poolMut sync.Mutex
+)
+
+func GetPool() *pgxpool.Pool {
+	if pool == nil {
+		slog.Debug("pool is nil, creating a new one")
+		poolMut.Lock()
+		defer poolMut.Unlock()
+
+		if pool == nil {
+			connString := config.DatabaseURL()
+			tmpPool, err := pgxpool.New(context.Background(), connString)
+			if err != nil {
+				panic(err)
+			}
+			pool = tmpPool
+			slog.Info("New pool has created")
+		}
+	}
+	return pool
+}
