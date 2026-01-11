11a48199099641e0ca7f773194f5a4ac3c8d951c
Daniel Kulichkov <danchick03@gmail.com>
2026-01-11 19:02:55 +0300

    feat(front): add fields to login form

--- apps/front/src/features/login/ui/LoginForm.tsx
+++ apps/front/src/features/login/ui/LoginForm.tsx
@@ -1,11 +1,20 @@
 'use client';
 
-import { Form } from '@/components/ui/form';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { SubmitHandler, useForm } from 'react-hook-form';
 import z from 'zod';
 import axios from 'axios';
 import { Button } from '@/components/ui/button';
+import { useRouter } from 'next/navigation';
+import { Input } from '@/components/ui/input';
 
 export const loginFormSchema = z.object({
   username: z.string('Необходимо заполнить'),
@@ -15,24 +24,56 @@ export const loginFormSchema = z.object({
 export type LoginFormData = z.infer<typeof loginFormSchema>;
 
 export function LoginForm() {
+  const router = useRouter();
   const form = useForm<LoginFormData>({
     resolver: zodResolver(loginFormSchema),
     defaultValues: {
-      username: 'admin',
-      password: 'topsecret',
+      username: '',
+      password: '',
     },
   });
   const onSubmit: SubmitHandler<LoginFormData> = (data) => {
-    axios.post('/api/auth/login', data).then(({ data }) => {
-      const { accessToken } = data;
-      localStorage.setItem('accessToken', accessToken);
+    axios.post('/api/auth/login', data).then(() => {
+      router.push('/');
     });
     form.reset();
   };
 
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="grid gap-4">
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>
+                Имя пользователя<span className="text-red-500">*</span>
+              </FormLabel>
+              <FormControl>
+                <Input placeholder="admin" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>
+                Пароль<span className="text-red-500">*</span>
+              </FormLabel>
+              <FormControl>
+                <Input type="password" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
         <Button type="submit">Войти</Button>
       </form>
     </Form>
