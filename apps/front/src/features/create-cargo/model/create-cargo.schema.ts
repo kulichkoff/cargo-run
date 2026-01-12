92a19f814338f8df51575e7dcbb045014d2b0921
Daniel Kulichkov <danchick03@gmail.com>
2026-01-12 22:58:52 +0300

    fix(cargo): improve zod validation for create cargo

--- apps/front/src/features/create-cargo/model/create-cargo.schema.ts
+++ apps/front/src/features/create-cargo/model/create-cargo.schema.ts
@@ -1,13 +1,15 @@
 import z from 'zod';
 
+const numericRegex = /^\d+$/g;
+
 export const createCargoSchema = z.object({
-  employeeId: z.string(),
-  vehicleId: z.string(),
-  startDate: z.date(),
-  deadlineDate: z.date(),
+  employeeId: z.string().regex(numericRegex, 'Некорректный ввод'),
+  vehicleId: z.string().regex(numericRegex, 'Некорректный ввод'),
+  customerId: z.string().regex(numericRegex, 'Некорректный ввод'),
+  startDate: z.date('Обязательное поле'),
+  deadlineDate: z.date('Обязательное поле'),
   price: z.string(),
   addressSequence: z.string(),
-  customerId: z.string(),
 });
 
 export type CreateCargoFormData = z.infer<typeof createCargoSchema>;
