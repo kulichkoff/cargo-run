3e25b4c4c0bf15094525ad1b5429de51dca46a22
Daniel Kulichkov <danchick03@gmail.com>
2026-01-12 17:10:17 +0300

    feat(api/cargo): add indexeds on employee and vehicle

---
+++ database/migrations/000002_create_cargo_indexes.down.sql
@@ -0,0 +1,2 @@
+DROP INDEX idx_cargo_vehicle_id;
+DROP INDEX idx_cargo_employee_id;
---
+++ database/migrations/000002_create_cargo_indexes.up.sql
@@ -0,0 +1,2 @@
+CREATE INDEX idx_cargo_vehicle_id ON cargos(vehicle_id);
+CREATE INDEX idx_cargo_employee_id ON cargos(employee_id);
