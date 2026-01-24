c23ffa07551e167edd581aeb366704b7858843be
Daniel Kulichkov <danchick03@gmail.com>
2026-01-24 15:08:54 +0300

    feat(front): add customers actions

--- apps/front/src/entities/customer/model/customer.columns.tsx
+++ apps/front/src/entities/customer/model/customer.columns.tsx
@@ -1,6 +1,15 @@
 import { ColumnDef } from '@tanstack/react-table';
 import { CustomerModel } from './customer.model';
 import { getCustomerTypeLocale } from '../lib';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import { Button } from '@/components/ui/button';
+import { MoreHorizontal } from 'lucide-react';
 
 export const customersColumns: ColumnDef<CustomerModel>[] = [
   {
@@ -17,5 +26,29 @@ export const customersColumns: ColumnDef<CustomerModel>[] = [
   {
     accessorKey: 'ogrn',
     header: 'ОГРН',
+  },
+  {
+    id: 'actions',
+    enableHiding: false,
+    cell: ({ row }) => {
+      //const customer = row.original;
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="h-8 w-8 p-0">
+              <span className="sr-only">Открыть меню</span>
+              <MoreHorizontal />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>Действия</DropdownMenuLabel>
+            <DropdownMenuItem disabled>Редактировать</DropdownMenuItem>
+            <DropdownMenuItem disabled>Удалить</DropdownMenuItem>
+            <DropdownMenuItem disabled>Просмотреть заказы</DropdownMenuItem>
+            <DropdownMenuItem disabled>Финансовые операции</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
+    },
   },
 ];
