3e145a24a39f34ff3c5ae50a460d0e74cee30170
Daniel Kulichkov <danchick03@gmail.com>
2026-01-25 16:42:41 +0300

    fix(api/delivery): change delivery order

--- apps/api/db/sqlc/delivery.sql.go
+++ apps/api/db/sqlc/delivery.sql.go
@@ -164,7 +164,7 @@ FROM delivery d
 LEFT JOIN driver e ON e.id = d.driver_id
 LEFT JOIN truck v ON v.id = d.truck_id
 LEFT JOIN customer c ON c.id = d.customer_id
-ORDER BY d.created_at
+ORDER BY d.created_at DESC
 LIMIT $1 OFFSET $2
 `
 
